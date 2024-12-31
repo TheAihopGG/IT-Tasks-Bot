209ff418ab7f83ece60ac39858d96c9eeb565c9b
TheAihopGG <108916224+TheAihopGG@users.noreply.github.com>
2024-12-31 09:01:51 +0700

    feat: added some poles

--- services/typed_dicts.py
+++ services/typed_dicts.py
@@ -2,5 +2,9 @@ from typing import TypedDict
 
 class TypedTask(TypedDict):
     id: int
+    image_url: str
     title: str
-    text: str
+    topic: str
+    task: str
+    answer: str
+    tags: str
